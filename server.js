93f3cf8589c9a7e796837242e0813433188e8eb9
Joseph White <josephwhite@Josephs-MacBook-Pro.local>
2021-09-28 17:12:25 -0600

    rock the vote git

--- server.js
+++ server.js
@@ -4,23 +4,15 @@ require("dotenv").config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const expressJwt = require('express-jwt')
+const secret = process.env.SECRET || "some secret passphrase here for local development"
 
 app.use(express.json())
 app.use(morgan('dev'))
  
-mongoose.connect(
-    'mongodb://localhost:27017/rock-the-vote',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true, 
-        useFindAndModify: false
-      },
-      () => console.log('Connected to the DB')
-    )
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
     
     app.use('/auth', require('./routes/authRouter'))
-    app.use("/api", expressJwt({secret: process.env.SECRET, algorithms: ['sha1', 'RS256', 'HS256']}))
+    app.use("/api", expressJwt({secret: secret, algorithms: ['sha1', 'RS256', 'HS256']}))
     app.use('/api/post', require('./routes/postRouter.js'))
     app.use('/api/user', require('./routes/userRouter.js'))
     app.use('/api/likeDislike', require('./routes/likeDislikeRouter.js'))
@@ -34,6 +26,19 @@ mongoose.connect(
       return res.send({errMsg: err.message})
     })
 
-app.listen(8000, () => {
-    console.log("Server running on Port 8000")
+      // ... other imports 
+  const path = require("path")
+
+  // ... other app.use middleware 
+  app.use(express.static(path.join(__dirname, "client", "build")))
+
+  // ...
+  // Right before your app.listen(), add this:
+  app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+
+  const port = process.env.PORT || 7000
+app.listen(port, () => {
+    console.log(`Server running on Port ${port}`)
 })
